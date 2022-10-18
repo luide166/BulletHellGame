f8f61dd04fef3227aa5b539859d16740b4a073e8
luide166 <luide166@gmail.com>
2022-10-18 18:11:32 -0300

    Add player

---
+++ Assets/Scripts/CharacterHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using UnityEngine;
+
+public class CharacterHealth : MonoBehaviour
+{
+    [Header("Health System")]
+    [SerializeField] 
+    int currentHealth;
+
+    public virtual void TakeDamage(int _amount)
+    {
+        currentHealth -= _amount;
+    }
+
+    public virtual void SetHealth(int _MaxHealth)
+    {
+        currentHealth = _MaxHealth;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    Rigidbody2D rb;
+    Vector2 moveDirection;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        moveDirection = moveInput.normalized * speed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveDirection * Time.fixedDeltaTime);
+    }
+}
